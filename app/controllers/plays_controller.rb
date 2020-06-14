adccd2b2462be1a11dbf154f995b1f1d693b9739
Shuji3233 <gorio9999@gmail.com>
2020-06-15 07:10:27 +0900

    Add Navigation and Associations

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -11,11 +11,11 @@ before_action :find_plays, only: [:show, :edit, :update, :destroy]
   
 
   def new
-    @play = Play.new
+    @play = current_user.play.build
   end
 
   def create
-    @play = Play.new(play_params)
+    @play = current_user.play.build(play_params)
 
     if @play.save
       redirect_to root_path
